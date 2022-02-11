3358982c7619e3ab62672b820574225bdeff5a81
chuskito88 <93283390+chuskito88@users.noreply.github.com>
2022-02-11 15:02:04 +0100

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,43 @@
+const btnNew = document.getElementById("btnNew");
+btnNew.addEventListener("click",fetchNewPlayers);
+
+async function fetchNewPlayers() {
+  const nameField = document.getElementById("txtName").value;
+	const ageField = document.getElementById("txtAge").value;
+	const surnameField = document.getElementById("txtSurname").value;
+  const teamField = document.getElementById("txtTeam").value;
+  console.log(nameField + " " + ageField + " " + surnameField + " " + teamField);
+  
+  const newPlayer = {name: nameField, age: ageField, surname: surnameField, team: teamField};
+
+  const response = await fetch(
+    "https://furboh.herokuapp.com/players",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      mode: "no-cors",
+      body: JSON.stringify(newPlayer)
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const span = document.getElementById('playerSpan');
+      let player = data;
+      span.innerHTML = `${player._id} ${player.name} ${player.surname} ${player.age} ${player.team}`; 
+        
+    })
+    .catch((error) => console.log(error));
+}
+
+var contents2;
+var selectTeam= document.getElementById("txtTeam");
+
+let teams = ["REAL MADRID", "FC BARCELONA", "BAYERN MUNICH", "BORUSSIA DORTMUND", "CHELSEA"];
+
+for (let i = 0; i <= 4; i++) {
+    contents2 += "<option>" + teams[i] + "</option>";
+}
+selectTeam.innerHTML = contents2;
